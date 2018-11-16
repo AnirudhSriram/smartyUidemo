cd957f3ba817db3c8af21e0db4371cce29246eb4
Terrorbladezz <anirudhsriram1995@gmail.com>
2018-11-16 07:03:02 +0530

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent,
+    MainPanelComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
